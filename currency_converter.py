fdb389666f4492cdf2176f1ec319c1f5c6229ec1
Anton <malysh70@gmail.com>
2026-07-26 17:05:42 +0300

    ++ dynamic currency labels and API request

--- currency_converter.py
+++ currency_converter.py
@@ -1,38 +1,61 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox as mb, ttk
+import requests
 
-# Словарь  валют
 CURRENCIES = {
     "EUR": "Евро",
-    "USD": "Доллар США",
+    "USD": "Американский доллар",
     "RUB": "Российский рубль",
-    "GBP": "Фунт UK",
+    "GBP": "Фунт стерлингов",
     "JPY": "Японская иена",
 }
 
-# --- Главное окно ---
+
+def update_currency_names(event=None):
+
+    label_name_base1.config(text=CURRENCIES.get(cb_base1.get(), ""))
+    label_name_base2.config(text=CURRENCIES.get(cb_base2.get(), ""))
+    label_name_target.config(text=CURRENCIES.get(cb_target.get(), ""))
+
+
+def fetch_exchange_rate(base_code, target_code):
+
+    url = f"https://open.er-api.com/v6/latest/{base_code}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("result") == "success":
+            rates = data.get("rates", {})
+            return rates.get(target_code)
+    return None
+
+
+# --- СОЗДАНИЕ ОКНА ---
 window = tk.Tk()
 window.title("Курсы обмена валют")
 window.geometry("300x420")
 
-# 1. базовая валюта №1
+# 1 Базовая валюта №1
 tk.Label(window, text="Базовая валюта", font=("Arial", 10)).pack(pady=(15, 5))
 cb_base1 = ttk.Combobox(
     window, values=list(CURRENCIES.keys()), state="readonly"
 )
 cb_base1.pack()
 cb_base1.set("EUR")
+cb_base1.bind("<<ComboboxSelected>>", update_currency_names)
 
 label_name_base1 = tk.Label(window, text=CURRENCIES["EUR"], fg="gray")
 label_name_base1.pack(pady=(2, 10))
 
-# 2. Вторая базовая валюта
+# 2. Базовая валюта №2
 tk.Label(window, text="Вторая базовая валюта", font=("Arial", 10)).pack(pady=5)
 cb_base2 = ttk.Combobox(
     window, values=list(CURRENCIES.keys()), state="readonly"
 )
 cb_base2.pack()
 cb_base2.set("USD")
+cb_base2.bind("<<ComboboxSelected>>", update_currency_names)
 
 label_name_base2 = tk.Label(window, text=CURRENCIES["USD"], fg="gray")
 label_name_base2.pack(pady=(2, 10))
@@ -44,6 +67,7 @@ cb_target = ttk.Combobox(
 )
 cb_target.pack()
 cb_target.set("RUB")
+cb_target.bind("<<ComboboxSelected>>", update_currency_names)
 
 label_name_target = tk.Label(window, text=CURRENCIES["RUB"], fg="gray")
 label_name_target.pack(pady=(2, 15))
